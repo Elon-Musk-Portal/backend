eb3f9e6520e4543c7b89b0e27e5ab3e1974c311c
Dzhos Oleksii <oleksii.dzhos@ukma.edu.ua>
2022-03-29 12:40:32 +0300

    EndPoint put tags to tweet

--- src/main/kotlin/com/elon_musk_portal/backend/tracks/tweets/TweetController.kt
+++ src/main/kotlin/com/elon_musk_portal/backend/tracks/tweets/TweetController.kt
@@ -1,8 +1,7 @@
 package com.elon_musk_portal.backend.tracks.tweets
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.elon_musk_portal.backend.tags.TagValue
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/tweets")
@@ -13,5 +12,28 @@ class TweetController(
     @GetMapping
     fun getTweets(): List<Tweet> {
         return tweetRepository.findAll()
+    }
+
+    @PutMapping("/{id}/tags")
+    fun addTagsToTweet(
+        @PathVariable
+        id: Long,
+        @RequestBody
+        tags: List<String>,
+    ): Tweet {
+        val tweet = tweetRepository.findById(id)
+        if (tweet.isEmpty) throw IndexOutOfBoundsException("Tweet with id $id not found")
+        else {
+            tags.forEach { tagValue ->
+                val allTags = TagValue.values().map { it.name }
+                if(!allTags.contains(tagValue)) {
+                    throw IndexOutOfBoundsException("Tag with name $tagValue didnt exist")
+                }
+            }
+            val updateTweet = tweet.get().copy(
+                tags = tags.joinToString(", ")
+            )
+            return tweetRepository.save(updateTweet)
+        }
     }
 }
