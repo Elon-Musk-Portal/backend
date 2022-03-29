d54e2413d6a34949d87465f2b15f7ff5335ac501
Dzhos Oleksii <oleksii.dzhos@ukma.edu.ua>
2022-03-29 21:29:34 +0300

    cors mapping

---
+++ src/main/kotlin/com/elon_musk_portal/backend/spring/WebSecurity.kt
@@ -0,0 +1,21 @@
+package com.elon_musk_portal.backend.spring
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebSecurity : WebSecurityConfigurerAdapter(), WebMvcConfigurer {
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests().antMatchers("/**").permitAll()
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("*");
+    }
+}
